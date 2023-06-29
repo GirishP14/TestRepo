8297502b078a215bfbe455cbee59cc14d638b17b
Girish <girir@LAPTOP-SM9BOFAC>
2023-06-29 21:53:12 +0530

    My first commit

---
+++ FactoryPattern/Implementation.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString() => GetType().Name;
+    }
+
+    /// <summary>
+    /// Concreate Product
+    /// </summary>
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch(_countryIdentifier)
+                {
+                    case "IN":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+
+
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _codeIdentifier;
+        public CodeDiscountService(Guid countryIdentifier)
+        {
+            _codeIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get => 15;
+        }
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    public class CountryDiscountFactory: DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string CountryIdentifier)
+        {
+            _countryIdentifier = CountryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryPattern;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factories = new List<DiscountFactory>
+            {
+                new CodeDiscountFactory(Guid.NewGuid()),
+                new CountryDiscountFactory("IN")
+            };
+
+            foreach (DiscountFactory disfact in factories)
+            {
+                var discountService = disfact.CreateDiscountService();
+                Console.WriteLine($"Percentage: {discountService.DiscountPercentage}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
